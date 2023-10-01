24984f55b9dc4f39734d28aea2960de6b51f4b3e
ngovanan-04 <22t1020006@husc.edu.vn>
2023-10-01 23:19:51 +0700

    update buoi7

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 # d3 = {'a': 10, 'b': 4, 'c': 1000}
 #
 # res ={**d1, **d2, **d3}
+from copy import deepcopy
+numbers = [[2, [45, 65]], [24, 98]]
+distore = {"an": 23, "uyen": 24, "anh": 22}
+setbb = {2, 6, 7, 3}
+sums = {2, 3, 5, 4, 8}
+sumj = {20, 54, 80}
 
-numbers = [1, 4, 6, 8, 7, 5]
-
-sth=max(numbers)
-print(sth)
+set3 = setbb.symmetric_difference(sums)
+print(set3)
+print(sums ^ setbb)
